9624d922f9eb15e89111692bfa65750fb3c28151
yatchenko-oleg <yatchenko-oleg89@mail.ru>
2022-10-16 11:12:35 +0300

    решил задачи

---
+++ Task 27/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+Console.WriteLine("Введите число А ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int B = int.Parse(Console.ReadLine());
+int Exponentiation()
+{
+    int result=1;
+    for (int i=1; i<=B; i++)
+    {
+        result=result*A;
+    }
+    return result;
+
+}
+int exp=Exponentiation();
+Console.WriteLine("Возведение числа А в степень В = " + exp);
---
+++ Tast 25/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+Console.WriteLine("Введите число N ");
+int N= int.Parse(Console.ReadLine());
+int SumNumber()
+{
+    int count=Convert.ToString(N).Length;
+    int variable = 0;
+    int result = 0;
+    
+for (int i=0; i<count; i++)
+{
+    count = N-N%10;
+    result=result + (N-count);
+    N=N/10;
+}
+return result; 
+}
+int sumN=SumNumber();
+Console.WriteLine(sumN);
